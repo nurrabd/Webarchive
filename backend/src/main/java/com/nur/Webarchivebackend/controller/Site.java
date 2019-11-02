ed2819e0e9ab3a88cc71c127dab8712122568fa9
nurhusein <nurabd@student.chalmers.se>
2019-11-02 16:41:07 +0100

    controller class for site is been added

--- backend/src/main/java/com/nur/Webarchivebackend/controller/Site.java
+++ backend/src/main/java/com/nur/Webarchivebackend/controller/Site.java
@@ -1,20 +1,22 @@
 package com.nur.Webarchivebackend.controller;
+
 import com.nur.Webarchivebackend.model.User;
 import com.nur.Webarchivebackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("api/user")
-public class user {
+@RequestMapping("api/site")
+public class Site {
 
     @Autowired
     private UserService userService;
 
     @GetMapping(path = "all")
-    public List<User> getAll(){
+    public List<Site> getAll(){
        return userService.getAll();
     }
 
